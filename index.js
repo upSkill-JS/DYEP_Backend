3c65a5bede12447f54f21883dfe0825e0abc6951
Kailash Chandra Bhakta <kcbhakta.99@gmail.com>
2022-01-24 02:11:26 +0530

    Code debugged by adding bodyParser middleware for json request

--- index.js
+++ index.js
@@ -5,21 +5,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import JobModel from './models/jobModel.js';
 
-// (1) To create a post route for Admin --- for creating job application in mongo DB database.
-/* -- importing npm package
--- connection to mongoDB
--- environmental variable // SIGNIFICANCE
->>> Geting the port from environmental variable... 
->>> Securing password and username...
-
--- post route build...
->>> FOLDER STRUCTURE...
+/* Bug resolved after adding middleware app.use(bodyParser.json({ extended: true }))
+-- Initially It was my missconception that urlencoded middleware is capable of parsing json post request,
+I was wrong we explicitly need bodyParser.json({ extended: true }) to parse incoming json request
 */
 
 const app = express();
 
 dotenv.config();
 
+app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
@@ -31,8 +26,6 @@ mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifie
     
 app.post('/job', async (req, res) => {
     const { title, job_profile } = req.body;
-    console.log(title);
-    
 
     const newJobModel = new JobModel({ title, job_profile });
 
